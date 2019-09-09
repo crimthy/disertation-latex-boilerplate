12139fb0d7a6d4cd5934b49e96e2e105e2bc0a8c
rostegg <rostik.nikolaenko@gmail.com>
2019-09-09 21:16:42 +0300

    🚗

--- scripts/build-pdf.js
+++ scripts/build-pdf.js
@@ -2,10 +2,10 @@ const { exec } = require('child_process')
 const fs = require('fs')
 const path = require('path')
 const directory =  path.join(__dirname, '../build')
-const fileName = process.argv[2] || 'example.tex'
+const fileName = process.argv[2] || 'main.tex'
 !fs.existsSync(directory) && fs.mkdirSync(directory)
 
-exec(`pdflatex -output-directory=${directory} -output-format=pdf -synctex=1 -interaction=nonstopmode ${fileName}`, (err, stdout, stderr) => {
+exec(`pdflatex -output-directory=${directory} -output-format=pdf -synctex=1 -interaction=nonstopmode -shell-escape ${fileName}`, (err, stdout, stderr) => {
   if (err) {
     console.log(`${err}`)
     return
--- scripts/linux/build.sh
+++ scripts/linux/build.sh
@@ -4,5 +4,5 @@ DELIM_COUNT="$(echo "${SCRIPT_DIR}" | awk -F"/" '{print NF-1}')"
 COUNT=$(( DELIM_COUNT - 1 ))
 BUILD_PATH="$(echo "$SCRIPT_DIR" | cut -d"/" -f1-${COUNT} )/build"
 FORMAT=${1}
-FILE_NAME=${2:-example.tex}
-mkdir -p build && pdflatex -output-directory=$BUILD_PATH -output-format=pdf -synctex=1 -interaction=nonstopmode $FILE_NAME
+FILE_NAME=${2:-main.tex}
+mkdir -p build && pdflatex -output-directory=$BUILD_PATH -output-format=pdf -synctex=1 -interaction=nonstopmode -shell-escape $FILE_NAME
