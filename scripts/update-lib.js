2f4fc5f51e86614766f3d642bc07ba60e6c41a26
rostegg <rostik.nikolaenko@gmail.com>
2019-10-29 21:16:21 +0200

    🆒

--- scripts/update-lib.js
+++ scripts/update-lib.js
@@ -2,33 +2,78 @@ const request = require('request-promise')
 const fs = require('fs')
 const path = require('path')
 const crypto = require('crypto')
+var https = require('https')
 
 const masterName = 'disertation-latex-boilerplate'
 const masterOwner = 'crimthy'
 const githubApiEndpoint = 'https://api.github.com/repos'
 const root =  path.join(__dirname, '../')
 
-const indexFiles = (target_path) => {
+const hashFile = targetFile  => {
+    return new Promise(function(resolve, reject) {
+        fs.createReadStream(targetFile).
+                pipe(crypto.createHash('sha1').setEncoding('hex')).
+                on('finish', function () {
+                    hash = this.read()
+                    resolve(hash)
+            })
+    })
+}
+const libIndex = (dirPath, result) => {
+    result = result || []
+    const targetPath = path.join(root,dirPath)
+    fs.readdirSync(targetPath).forEach(file => {
+        const targetFile = path.join(targetPath,file)
+        if (fs.lstatSync(targetFile).isDirectory())
+            libIndex(path.join(dirPath, file), result)
+        else
+            result.push(path.join(dirPath, file))
+    })
+    return result
+}
+
+const indexFiles = (targetPath) => {
     const options = {
         method: 'GET',
-        uri: `${githubApiEndpoint}/${masterOwner}/${masterName}/contents/${target_path}`,
+        uri: `${githubApiEndpoint}/${masterOwner}/${masterName}/contents/${targetPath}`,
         headers: {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.70 Safari/537.36',
             'Accept': 'application/vnd.github.v3+json'
         }
     }
+
     request(options)
         .then(function (response) {
             const responseObject = JSON.parse(response)
 
             const files  = responseObject.filter(element => element['type'] === 'file')
             files.forEach(element => {
-                fs.createReadStream(path.join(root,element['path'])).
-                    pipe(crypto.createHash('sha1').setEncoding('hex')).
-                    on('finish', function () {
-                        hash = this.read()
-                        console.log(`File: ${element['path']}\nGithub: ${element['sha']}\nSelf: ${hash}\n\n`)
-                })
+                const filePath = path.join(root,element['path'])
+                try {
+                    if (fs.existsSync(filePath)) {
+                        hashFile(filePath).then(function(localFileHash) {
+                            const tempPath = path.join(__dirname, localFileHash)
+                            const file = fs.createWriteStream(tempPath)
+                            https.get(element['download_url'], function(response) {
+                                response.pipe(file)
+                                hashFile(tempPath).then(function(downloadedFileHash) {
+                                    if (localFileHash !== downloadedFileHash) {
+                                        console.log(`File: ${element['path']} has new version, replacing...`)
+                                        response.pipe(filePath)
+                                        //fs.writeFileSync(filePath,response)
+                                    }
+                                    fs.unlinkSync(tempPath)
+                                })
+                            }) 
+                        })
+                    }
+                }
+                catch(err) {
+                    const file = fs.createWriteStream(filePath)
+                    https.get(element['download_url'], function(response) {
+                        response.pipe(file)
+                    })
+                }
             })
 
             responseObject.
